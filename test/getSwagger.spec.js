6f99fbe04cb05acd7b7887984c9ea659e2026016
Preston Van Loon <preston@machinepowered.com>
2016-04-14 14:59:51 -0500

    added test for files

--- test/getSwagger.spec.js
+++ test/getSwagger.spec.js
@@ -12,6 +12,7 @@ const getSwagger = require('../lib/getSwagger');
 const res = fs.readFileSync('test/swagger.yaml', 'utf8');
 
 const url = 'https://example.com/swagger.yaml';
+const file = 'dist/swagger.yaml';
 const response = {
   swagger: '2.0'
 };
@@ -20,17 +21,26 @@ describe('getSwagger', () => {
   beforeEach(() => {
     sinon.stub(request, 'get').resolves(res);
     sinon.stub(yaml, 'safeLoad').returns(response);
+    sinon.stub(fs, 'readFileSync').returns(res);
   });
 
   afterEach(() => {
     request.get.restore();
     yaml.safeLoad.restore();
+    fs.readFileSync.restore();
   });
 
   it('should call request.get with url', () => {
     return getSwagger(url).then(() => {
       expect(request.get.called).to.be.true;
       expect(request.get.firstCall.args[0]).to.equal(url);
+    });
+  });
+
+  it('should work with files', function () {
+    return getSwagger(file).then(result => {
+      expect(fs.readFileSync.called).to.be.true;
+      expect(fs.readFileSync.firstCall.args[0]).to.equal(file);
     });
   });
 
@@ -41,6 +51,4 @@ describe('getSwagger', () => {
       expect(result).to.deep.equal(response);
     });
   });
-
-  it('should work with files');
 });
